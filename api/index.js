3479c936eb7d08957d59b0bf36a45006c00c6bbb
Luciano-Paniccia <84297226+Luciano-Paniccia-Git@users.noreply.github.com>
2022-09-26 17:15:28 -0300

    deployar

--- api/index.js
+++ api/index.js
@@ -26,3 +26,5 @@ conn.sync({ force: true }).then(() => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
+const { PORT } = process.env;
+server.listen(PORT, () => console.log(`Listen on port ${PORT}`));
