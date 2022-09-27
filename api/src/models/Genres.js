e5bb696554be24c658503e771be7278ab4ba2e4b
Luciano-Paniccia <84297226+Luciano-Paniccia-Git@users.noreply.github.com>
2022-09-27 17:23:32 -0300

    fin?

--- api/src/models/Genres.js
+++ api/src/models/Genres.js
@@ -4,7 +4,7 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
 	//defino el modelo de género
 	sequelize.define(
-		"Genres", {
+		"genres", {
 		id: {
 			type: DataTypes.UUID,
 			defaultValue: DataTypes.UUIDV4,
--- client/src/components/CardVideogame/CardVideogame.jsx
+++ client/src/components/CardVideogame/CardVideogame.jsx
@@ -10,7 +10,7 @@ class CardVideogame extends React.Component {
                 <img src={this.props.image} width="400px" height="250px" alt=""/>
                 <div className={s.card__content}>
                     <h3 className={s.nombre}>{this.props.name}</h3>
-                    <p className={s.genres}>{this.props.genres}</p>
+                    <h4 className={s.genres}>{this.props.genres}</h4>
                     <p className={s.rating}>⭐ {this.props.rating}</p>
                  <  NavLink to={`/detail/${this.props.id}`} className={s.navLink}><span className={s.leer_mas}>Leer más</span></NavLink>
              </div>
